9419463518395fa55e89befc34f67765fa4b5551
jobayermannan <jobayermannan777@gmail.com>
2023-12-26 17:56:10 +0600

    impliment jwt and user and video schema

---
+++ src/models/user.model.js
@@ -0,0 +1,115 @@
+import mongoose ,{Schema, model} from "mongoose";
+import  jwt  from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+   username:{
+      type:String,
+      required:true,
+      lowercase:true,
+      trim:true,
+      unique:true,
+      index:true
+
+   },
+ email:{
+      type:String,
+      required:true,
+      lowercase:true,
+      trim:true,
+      unique:true,
+     
+
+   },
+ fullName:{
+      type:String,
+      required:true,
+      
+      trim:true,
+      index:true
+
+   },
+     avatar:{
+      type:String,//cloudinary only
+      required:true,
+      
+      
+      
+
+   },
+     coverImage:{
+      type:String,//cloudinary only
+     
+      
+      
+      
+
+   },
+   
+    watchHistory:[{
+      type:Schema.Types.ObjectId,
+      ref:"Video"
+    }],
+    password:{
+      type:String,
+      required:[true, "password is required"],
+      trim:true,
+      minlength:6,
+      select:false
+    },
+    refreshToken:{
+      type:String,
+     
+    }
+
+   
+     
+     
+
+   
+
+},{timestamps:true});
+
+userSchema.pre("save", async function(next){
+   if(!this.isModified("password")) return next();
+          this.password =bcrypt.hash(this.password,12) 
+   next()
+})
+userSchema.methods.isPasswordCorrect= async function (password){
+   return await  bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=async function(){
+   return  jwt.sign(
+
+      {
+         _id: this._id,
+         email: this.email,
+         username:this.username,
+         fullName:this.fullName,
+         
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      },
+    )
+}
+userSchema.methods.generateRefreshToken=async function(){
+   return  jwt.sign(
+
+      {
+         _id: this._id,
+         email: this.email,
+         username:this.username,
+         fullName:this.fullName,
+         
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      },
+    )
+}
+
+export const User=model("User",userSchema)
